455760b82323a7f2edce6dc6b99d853c8c1ddfcf
name <email>
2020-02-09 11:13:27 +0900

    initial commit

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,7 +1,7 @@
 @TestOn('browser')
 
-import 'package:angular_app/app_component.dart';
-import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_dart_realworld_example_app/app_component.dart';
+import 'package:angular_dart_realworld_example_app/app_component.template.dart' as ng;
 import 'package:angular_test/angular_test.dart';
 import 'package:test/test.dart';
 
--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
 import 'package:angular/angular.dart';
-import 'package:angular_app/app_component.template.dart' as ng;
+import 'package:angular_dart_realworld_example_app/app_component.template.dart' as ng;
 
 void main() {
   runApp(ng.AppComponentNgFactory);
